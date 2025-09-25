6497e1faac007ccb3c06d34c90b4e11c6a2b207c
Ahsan8381 <ahsan8381amu@gmail.com>
2025-09-26 00:18:09 +0530

    made some changes to make it responsive

--- cgpa-calculator-main/landingpage.js
+++ cgpa-calculator-main/landingpage.js
@@ -1,31 +1,23 @@
-  // Show/hide CGPA definition
-  document.getElementById('showCgpaDefinition').addEventListener('click', function() {
-      const definition = document.getElementById('cgpaDefinition');
-      definition.style.display = 'block';
-      document.getElementById('showGradeTable').style.display = 'block'; // Show the next button
+// Show/hide CGPA definition
+document
+  .getElementById("showCgpaDefinition")
+  .addEventListener("click", function () {
+    document.getElementById("cgpaDefinition").style.display = "block";
+    document.getElementById("showGradeTable").style.display = "block";
   });
 
-  // Show/hide grade table
-  document.getElementById('showGradeTable').addEventListener('click', function() {
-      const gradeTable = document.getElementById('gradeTable');
-      gradeTable.style.display = 'block';
-      document.getElementById('showCalculationMethod').style.display = 'block'; // Show the next button
+// Show/hide grade table
+document
+  .getElementById("showGradeTable")
+  .addEventListener("click", function () {
+    document.getElementById("gradeTable").style.display = "block";
+    document.getElementById("showCalculationMethod").style.display = "block";
   });
 
-  // Show/hide calculation method
-  document.getElementById('showCalculationMethod').addEventListener('click', function() {
-      const calculationMethod = document.getElementById('calculationMethod');
-      calculationMethod.style.display = 'block';
-      document.getElementById('thankYouMessage').style.display = 'block'; // Show thank you message
+// Show/hide calculation method
+document
+  .getElementById("showCalculationMethod")
+  .addEventListener("click", function () {
+    document.getElementById("calculationMethod").style.display = "block";
+    document.getElementById("thankYouMessage").style.display = "block";
   });
-
-  function getQueryParam(param) {
-      const urlParams = new URLSearchParams(window.location.search);
-      return urlParams.get(param);
-  }
-
-
-  const name = getQueryParam('name');
-
-
-  document.getElementById('displayName').textContent = name;
